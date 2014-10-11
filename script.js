c3d374743d3577493c61c08e1871604d35cd128b
ttante <tylerdinner@gmail.com>
2014-10-11 11:18:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+
+
+
+	console.log('script.js reporting!');
+
+
+	var targetMyDiv = document.getElementById('jsTarget');
+	var styleDiv = targetMyDiv.style;
+	 styleDiv.width = ('200px');
+	 styleDiv.height = ('100px');
+	 styleDiv.border = ('3px solid red');
+	 styleDiv.backgroundColor = ('forestgreen');
+
+
+
+
+}
